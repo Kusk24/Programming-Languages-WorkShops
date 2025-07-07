74b747f20e64644c9f80cb46f2958e4460ac3aed
Kusk24 <winyumg2003a@gmail.com>
2025-07-07 23:14:31 +0700

    Partially Finished WorkShop5

--- w5_6612054.ml
+++ w5_6612054.ml
@@ -3,3 +3,34 @@ Name - Win Yu Maung
 ID - 6612054
 Sec - 542
 *)
+
+(* 1 *)
+fun exist (e, []) = false
+| exist (e, L) = let
+                val x = hd L;
+                val xs = tl L;
+                in 
+                    if x = e then true 
+                    else exist (e, xs)
+                end;
+
+(* 2 *)
+fun greaterthan ([], e) = []
+| greaterthan (L, e) = let 
+                        val x = hd L;
+                        val xs = tl L;
+                    in if x > e 
+                    then x :: greaterthan (xs, e)
+                    end;
+
+(* 3 *)
+fun request([]) = false
+|request ([_]) = false
+| request (L) = let
+                val x = hd L;
+                val xs = tl L;
+                in 
+                  if x = (hd xs) then true
+                  else request (xs)
+                end;
+
